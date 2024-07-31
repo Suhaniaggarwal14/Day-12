1319161b9e99824ea04016a7597a1c24ea5f8f68
Suhaniaggarwal14 <140490968+Suhaniaggarwal14@users.noreply.github.com>
2024-07-31 19:47:12 +0200

    Add files via upload

---
+++ New folder/Day 13/demoOnSwitch.js
@@ -0,0 +1,12 @@
+//1:hi;2:hello;3:bye
+
+var n=1;
+switch(n){
+    case 1: console.log("hi")
+    break;
+    case 2:console.log("hello")
+    break;
+    case 3:console.group("bye")
+    break;
+    default:console.log("invalid input")
+}
---
+++ New folder/Day 13/palindrome.js
@@ -0,0 +1,21 @@
+// var num=156;
+// var temp=num;
+// var rem,rev=0;
+// while(num>0)
+// {
+//     rem=num%10;
+//     rev=rev+rem*10;
+//     num=num/10;
+//     num=Math.floor(num/10);
+
+// }
+// if(temp===rev)
+//     console.log("palindrome")
+// else
+//     console.log("not palindrome")
+
+var str='liril'
+console.log(str.split('').reverse().join('')==str)
+console.log("palindrome string")
+
+console.log("not paindrome string")
